48e63bf32ab6f31f80f3d990c59fe6838d1c5740
Divya Datla <ndatla3@gatech.edu>
2016-06-01 19:22:02 -0400

    Added MyCustomString and added empty methods

---
+++ Assignment3/src/edu/gatech/seclass/MyCustomString.java
@@ -0,0 +1,36 @@
+package edu.gatech.seclass;
+
+public class MyCustomString implements MyCustomStringInterface{
+
+	@Override
+	public void setString(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEveryNthCharacter(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int countDigits() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void convertDigitsToNamesInSubstring(int startPosition, int endPosition)
+			throws MyIndexOutOfBoundsException, IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
